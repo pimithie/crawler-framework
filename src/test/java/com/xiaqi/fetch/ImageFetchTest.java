faf3e0fe6b959aac10d3a5ebdf5281cdcbdf1ff4
pimithie <xq262762708@aliyun.com>
2020-03-16 22:52:17 +0800

    add the image fetcher test case

--- src/test/java/com/xiaqi/fetch/ImageFetchTest.java
+++ src/test/java/com/xiaqi/fetch/ImageFetchTest.java
@@ -1,6 +1,27 @@
 package com.xiaqi.fetch;
 
+import com.xiaqi.crawl.fetcher.ImageFetcher;
+import com.xiaqi.crawl.filter.fetch.FetchMetaData;
+import com.xiaqi.crawl.utils.URLUtils;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.io.IOException;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring-mvc.xml")
 public class ImageFetchTest {
+
+    @Autowired
+    private ImageFetcher imageFetcher;
+
+    @Test
+    public void test() throws IOException {
+        final String imageUrl = "";
+        FetchMetaData fetchMetaData = URLUtils.getFetchMetaData(imageUrl);
+        imageFetcher.fetch(fetchMetaData);
+    }
 }
